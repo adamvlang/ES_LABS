264422d10fdd3d00e036fe4b5d76a6b63351dbec
adamvlang <adamlang@kth.se>
2015-12-08 16:29:42 +0100

    Started on labb 4, next step is to read temp

--- LAB4/CAN_LAB4/src/main.c
+++ LAB4/CAN_LAB4/src/main.c
@@ -15,8 +15,10 @@ CAN example
 
 UINT32 Ident;
 UINT8 msg[8], mSize;
+UINT16 light;
 
 int main(void) {
+	
 	//spidatareadpointer=&spidataread;
 	pm_switch_to_osc0(&AVR32_PM, FOSC0, OSC0_STARTUP);
 	
@@ -36,43 +38,55 @@ int main(void) {
 	dip204_init(100,1);
 	dip204_clear_display();
 	
-	UINT16 Mask = 0; 
-	UINT16 flt = 0;
+	UINT16 Mask = 0xF000; 
+	UINT16 flt = 0x2000;
 	UINT16 Flt[] = {flt,flt,flt,flt,flt,flt};
-	InitializeCAN(0, CAN_250kbps, Mask, Flt);
+	InitializeCANExtended(0, CAN_250kbps, Mask, Flt);
+	
 	
 
-
 	while(1){
+		
 		//Clear memory contents
 		ClearMessages(msg);
 		//Read any message available
 		if(CANRxReady(0)){
 			if( CANGetMsg(0, &Ident, msg, &mSize )) // Gets message and returns //TRUE if message received.
 			{	
-
-				if( Ident == 0x00001000)
-				{
+					// Read light, 2 most sig byte 2, 8 least sig byte 3
+					light = (((UINT16)msg[2]) << 8 ) | msg[3];
+					// Read temp
+					
 					dip204_clear_display();
 					dip204_set_cursor_position(1, 1);
 					dip204_printf_string("Rx ID:");
-					dip204_set_cursor_position(1,2);
+					dip204_set_cursor_position(1, 2);
 					dip204_printf_string("Temp:");
 					dip204_set_cursor_position(1, 3);
 					dip204_printf_string("Light:");
 					dip204_set_cursor_position(1, 4);
 					dip204_printf_string("Pot:");
-					dip204_hide_cursor();
+					dip204_set_cursor_position(13, 2);
+					dip204_printf_string("Abs:");
+					dip204_set_cursor_position(13, 3);
+					dip204_printf_string("Abs:");
+					dip204_set_cursor_position(13, 4);
+					dip204_printf_string("Abs:");
 					dip204_set_cursor_position(9, 1);
 					dip204_printf_string("%x", Ident);
 					dip204_set_cursor_position(9, 2);
 					dip204_printf_string("%d", msg[1]);
 					dip204_set_cursor_position(9, 3);
-					dip204_printf_string("%d", msg[3]);
+					dip204_printf_string("%d", light);
 					dip204_set_cursor_position(9, 4);
-					dip204_printf_string("%d", msg[4]);
+					dip204_printf_string("%d", msg[5]);
+					dip204_set_cursor_position(18, 2);
+					dip204_printf_string("%x", msg[2]);
+					dip204_set_cursor_position(18, 3);
+					dip204_printf_string("%x", msg[3]);
+					dip204_set_cursor_position(18, 4);
+					dip204_printf_string("%d", msg[5]);
 					dip204_hide_cursor();
-				}
 			}
 			
 		}
